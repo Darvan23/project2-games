da66620554399fdd42c8ef4623630784744ab58d
ayad1119 <302237965@student.rocmondriaan.nl>
2024-11-17 21:21:46 +0100

    javascript code for the snake
    added the place where the snake will run.

--- game3/game3.js
+++ game3/game3.js
@@ -1,3 +1,30 @@
- {
+//board
+var blockSize = 25;
+var rows = 20;
+var cols = 20;
+var board;
+var context;
+
+//snake head
+//var snakeX = blockSize = 5;
+//var snakeY = blockSize = 5;
+
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = rows * blockSize;
+    board.width = cols * blockSize;
+    context = board.getContext("2d");
+
+    update();
+
+}
+
+function update() {
+    context.fillStyle = "black";
+    context.fillRect(0, 0, board.width, board.height);
+
+    context.fillStyle="lime";
+    context.fillRect(snakeX, snakeY , blockSize , blockSize);
 
 }
