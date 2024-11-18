7aa60d93901810ae641f55a1c756330bf86eae01
Darvan23 <302348745@student.rocmondriaan.nl>
2024-11-18 11:50:25 +0100

    project 2
    added for loop function

--- chess/chess.js
+++ chess/chess.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const chessboard = document.querySelector('#chessboard');
 
-    // Initial positions of the pieces on the board
+   
     const initialBoardSetup = [
         ['rookblack', 'knightblack', 'bishopblack', 'queenblack', 'kingblack', 'bishopblack', 'knightblack', 'rookblack'],
         ['pawnblack', 'pawnblack', 'pawnblack', 'pawnblack', 'pawnblack', 'pawnblack', 'pawnblack', 'pawnblack'],
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
         ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook']
     ];
 
-    // Function to create the chessboard
+    
     function createChessBoard() {
         for (let row = 0; row < 8; row++) {
             for (let col = 0; col < 8; col++) {
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 const piece = initialBoardSetup[row][col];
                 if (piece) {
                     const pieceImg = document.createElement('img');
-                    pieceImg.src = `img/${piece}.webp`; // Path to the image folder
+                    pieceImg.src = `img/${piece}.webp`; 
                     pieceImg.alt = piece;
                     pieceImg.classList.add('piece');
                     square.appendChild(pieceImg);
@@ -35,6 +35,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // Initialize the board
+   
     createChessBoard();
 });
