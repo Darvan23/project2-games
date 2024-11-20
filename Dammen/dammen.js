d81375bb44f73ea46c5f88ad32829d0aa11190df
WS-Saief <302521293@student.rocmondriaan.nl>
2024-11-20 10:27:38 +0100

    1

---
+++ Dammen/dammen.js
@@ -0,0 +1,11 @@
+const board = document.getElementById('board');
+
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+                const cell = document.createElement('div');
+                cell.classList.add('cell');
+                cell.setAttribute('data-row', i);
+                cell.setAttribute('data-col', j);
+                board.appendChild(cell);
+            }
+        }
